d902bfbdd2e27be6f0851df3e772d3efdbd4f6d6
yashwanth <yashwanthnapa@gmail.com>
2025-02-12 11:59:33 +0530

    initial commit

---
+++ ui-admin/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import AdminPanel from './AdminPanel.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <AdminPanel />
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
